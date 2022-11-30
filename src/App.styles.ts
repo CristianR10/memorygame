c22538530f33b4896898423db5a2c439f91c9786
Cristian Maeztra <cristian@maeztra.com>
2022-11-29 21:35:13 -0300

    feat: create button and grid card

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -49,3 +49,9 @@ export const GridArea = styled.div`
     margin: 0 20px;
   }
 `;
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from "react";
 import * as C from "./App.styles";
 import logoImage from "./assets/devmemory_logo.png";
+import RestartIcon from "./svgs/restart.svg";
+import { Button } from "./components/Button";
 import { InfoItem } from "./components/InfoItem";
+import { GridItemType } from "./types/GridItemType";
 
 const App = () => {
+  // Primeiro eu preciso saber se o jogos esta rodando
+  const [playing, setPlaying] = useState<boolean>(false);
+  // Tempo que passou
+  const [timeElapsed, setTimeElapsed] = useState<number>(0);
+  //Quantidade de movimento
+  const [moveCount, setMoveCount] = useState<number>(0);
+  // Quantas cartas estão exibindo
+  const [shownCount, setShownCount] = useState<number>(0);
+  // GRID
+  const [gridItems, setGridItems] = useState<GridItemType[]>([]);
+
+  useEffect(() => resetAndCreateGrid(), []);
+
+  // Criar e resetar os cards
+  const resetAndCreateGrid = () => {};
+
   return (
     <C.Container>
       <C.Info>
@@ -13,9 +33,16 @@ const App = () => {
           <InfoItem label="Tempo" value="00:00" />
           <InfoItem label="Movimento" value="0" />
         </C.InfoArea>
-        <button>Reiniciar</button>
+
+        <Button
+          label="Reiniciar"
+          icon={RestartIcon}
+          onClick={resetAndCreateGrid}
+        />
       </C.Info>
-      <C.GridArea>Teste</C.GridArea>
+      <C.GridArea>
+        <C.Grid></C.Grid>
+      </C.GridArea>
     </C.Container>
   );
 };
